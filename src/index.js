f2bc6bb256cfbf0a5a964f8ce0104f30bc7d9be0
Manuel Klarmann <mklarmann@eaternity.ch>
2018-02-01 18:58:25 +0100

    keep awake - not tested

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
 import { Root, Tabs } from "./config/router";
+import KeepAwake from "react-native-keep-awake";
 
 class App extends Component {
   render() {
-    return <Root />;
+    return (
+      <Root>
+        <KeepAwake />
+      </Root>
+    );
   }
 }
 
